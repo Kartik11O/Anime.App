bbb7746a0454122649c13e53e84dadd9027f6d4a
Kartik11O <125755557+Kartik11O@users.noreply.github.com>
2024-04-27 01:39:11 +0530

    p1

--- JS/Fetching.js
+++ JS/Fetching.js
@@ -52,16 +52,15 @@ async function Fetching(formData) {
 
 
 
-// document.getElementById('file').addEventListener('change', handleFileSelect);
-// function handleFileSelect(event) {
-//   const file = event.target.files[0];
-//   if (file) {
-//     removeNodes()
-//     GettingIMG()
-//   } else {
-//     console.log('No file selected');
-//   }
-// }
+document.getElementById('file').addEventListener('change', handleFileSelect);
+function handleFileSelect(event) {
+  const file = event.target.files[0];
+  if (file) {
+    GettingIMG()
+  } else {
+    console.log('No file selected');
+  }
+}
 // Add event listener for the 'keypress' event
 window.addEventListener('keypress', function (event) {
   if (event.key === 'Enter') {
